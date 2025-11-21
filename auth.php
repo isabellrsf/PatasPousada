187048e856ee68613695d970efb583de84f5b920
Isabella <isa.rodrigues01@hotmail.com>
2025-11-21 16:42:40 -0300

    Atualizacao

--- auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-// auth.php
-if (session_status() === PHP_SESSION_NONE) session_start();
-
-if (empty($_SESSION['profile_id'])) {
-  $self = basename($_SERVER['REQUEST_URI']);
-  header('Location: login.html?next=' . urlencode($self));
-  exit;
-}
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION = [];
-session_destroy();
-
-// se quiser, também pode limpar cookie específico de sessão aqui
-
-header('Location: cadastrarouentrar.html');
-exit;
